f795130031255ae878288a7e5f017454888142c5
Leye Oduola <90718905+olaleyeO01@users.noreply.github.com>
2021-10-20 12:02:58 +0100

    Add files via upload

---
+++ design_strategies_1/lib/remembering_names.rb
@@ -0,0 +1,6 @@
+def add_name(name)
+    array_of_names = []
+    name = gets.chomp
+    array_of_names << name
+    "person sorted"
+end
---
+++ design_strategies_1/spec/remebering_names_spec.rb
@@ -0,0 +1,7 @@
+require 'remembering_names'
+describe "class mates names" do
+    it "gives us people name" do
+      expect(add_name("name")).to eq "person sorted"
+    end
+  end
+  
